cf5f2fa9d7cdc904407d605fdcb4bd58abcd8fff
bodrius <bogdanpechenyk@gmail.com>
2022-04-11 19:56:27 +0300

    fix env

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const app = require('./app')
+const app = require("./app");
+require("dotenv").config();
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+console.log("process.env.PORT", process.env.PORT);
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log("Server running. Use our API on port: 8080");
+});
