36e245249788e3926515455679f1011dabd7ba54
bodrius <bogdanpechenyk@gmail.com>
2022-04-11 19:47:12 +0300

    fix

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(logger(formatsLogger));
 
